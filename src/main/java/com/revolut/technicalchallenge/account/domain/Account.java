94b4f03d5fd81f8dc447dea7e306670e7b1ab5a5
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2019-07-21 09:06:08 +0100

    tests: fix tests written with Spark-Test

--- src/main/java/com/revolut/technicalchallenge/account/domain/Account.java
+++ src/main/java/com/revolut/technicalchallenge/account/domain/Account.java
@@ -4,6 +4,7 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @DatabaseTable(tableName = "accounts")
 public class Account {
@@ -18,6 +19,12 @@ public class Account {
   private BigDecimal amount = BigDecimal.ZERO;
 
   public Account() {
+  }
+
+  public Account(int id, String owner, BigDecimal amount) {
+    this.id = id;
+    this.owner = owner;
+    this.amount = amount;
   }
 
   public int getId() {
@@ -42,5 +49,20 @@ public class Account {
 
   public void setAmount(BigDecimal amount) {
     this.amount = amount;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Account)) return false;
+    Account account = (Account) o;
+    return getId() == account.getId() &&
+      Objects.equals(getOwner(), account.getOwner()) &&
+      Objects.equals(getAmount(), account.getAmount());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId(), getOwner(), getAmount());
   }
 }
